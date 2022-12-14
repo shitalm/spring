e7ffbb58e70e8fe997ae83364637bdb38ef5f65f
Shital Mehta <shital.mehta@olacabs.com>
2022-12-14 13:06:18 +0530

    Introducing prod properties

---
+++ spring-jdbc/README.md
@@ -0,0 +1,2 @@
+## To specify a different location for application properties, specify it in commandline as follows
+<code>java --spring.config.location=file:///tmp/config/application.properties</code>
--- spring-jdbc/src/main/java/com/example/demo/DemoApplication.java
+++ spring-jdbc/src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -10,6 +11,8 @@ import org.springframework.context.event.EventListener;
 public class DemoApplication {
 	@Autowired
 	private CustomerService customerService;
+	@Value( "${app.version}" )
+	private String appVersion;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -17,6 +20,7 @@ public class DemoApplication {
 
 	@EventListener(ApplicationReadyEvent.class)
 	public void test() {
+		System.out.println("DemoApplication::test app.version=" + appVersion);
 		System.out.println("DemoApplication::test count=" + customerService.getCount());
 	}
 
